919d017d65824470ae2854c03a20967cc78c5d8e
ligang <2990875927@qq.com>
2019-12-16 23:11:09 +0800

    前端内容HTML

--- code_oldboy/数据库/SQLALchemy/基本增删改查.py
+++ code_oldboy/数据库/SQLALchemy/基本增删改查.py
@@ -112,3 +112,95 @@ user_typr_list = session.query(UserType.title).filter(UserType.id > 0). \
 
 session.commit()
 session.close()
+
+# 其他
+# 　条件
+# filter:表达式,filter_by:表示条件,相当于与传入一个字典
+# ret = session.query(Users).filter_by(name='alex').all()
+# ret = session.query(Users).filter(Users.id > 1, Users.name == 'eric').all()
+# ret = session.query(Users).filter(Users.id.between(1, 3), Users.name == 'eric').all()
+# ret = session.query(Users).filter(Users.id.in_([1,3,4])).all()
+# ret = session.query(Users).filter(~Users.id.in_([1,3,4])).all()
+# ret = session.query(Users).filter(Users.id.in_(session.query(Users.id).filter_by(name='eric'))).all()
+
+# from sqlalchemy import and_, or_
+# ret = session.query(Users).filter(and_(Users.id > 3, Users.name == 'eric')).all()
+# ret = session.query(Users).filter(or_(Users.id < 2, Users.name == 'eric')).all()
+# ret = session.query(Users).filter(
+#     or_(
+#         Users.id < 2,
+#         and_(Users.name == 'eric', Users.id > 3),
+#         Users.extra != ""
+#     )).all()
+
+
+# 通配符
+# ret = session.query(Users).filter(Users.name.like('e%')).all()
+# ret = session.query(Users).filter(~Users.name.like('e%')).all()
+
+# 限制
+# ret = session.query(Users)[1:2]
+
+# 排序
+# ret = session.query(Users).order_by(Users.name.desc()).all()
+# ret = session.query(Users).order_by(Users.name.desc(), Users.id.asc()).all()
+
+# 分组
+# from sqlalchemy.sql import func
+#
+# ret = session.query(Users).group_by(Users.extra).all()
+# ret = session.query(
+#     func.max(Users.id),
+#     func.sum(Users.id),
+#     func.min(Users.id)).group_by(Users.name).all()
+#
+# ret = session.query(
+#     func.max(Users.id),
+#     func.sum(Users.id),
+#     func.min(Users.id)).group_by(Users.name).having(func.min(Users.id) >2).all()
+
+# 连表
+# ret = session.query(Users, Favor) # 笛卡尔积,两张表不加条件
+# ret = session.query(Users, Favor).filter(Users.id == Favor.nid).all()
+# ret = session.query(Person).join(Favor).all()# innner join
+# ret = session.query(Person).join(Favor, isouter=True).all()# left join
+
+# 组合
+# q1 = session.query(Users.name).filter(Users.id > 2)
+# q2 = session.query(Favor.caption).filter(Favor.nid < 2)
+# ret = q1.union(q2).all()
+#
+# q1 = session.query(Users.name).filter(Users.id > 2)
+# q2 = session.query(Favor.caption).filter(Favor.nid < 2)
+# ret = q1.union_all(q2).all()
+
+# 子查询!!!!!
+# 1.
+# select * from b where id in (select id from tb2)
+
+# 2.
+# select * from (select * from tb) as B
+# subquery():要加上,指明它是一个子查询语句!!!
+# q1 = session.query(UserType).filter(UserType.id > 0).subquery()
+# result = session.query(q1).all()
+# print(result)
+
+# 3.
+# select
+#   id ,
+#   (select * from users where users.user_type_id=usertype.id)
+# from usertype;
+
+
+# as_scalar:表名这依然是一个SQL语句,而不是已经转化为一个表了!!!
+
+# session.query(UserType,session.query(Users).filter(Users.id == 1).subquery())
+# session.query(UserType,Users)
+# result = session.query(UserType.id,session.query(Users).as_scalar())
+# print(result)
+# result = session.query(UserType.id,session.query(Users).filter(Users.user_type_id==UserType.id).as_scalar())
+# print(result)
+
+
+# 表示当循环到某个用户类型后,拿着用户类型的ID去另一个表里遍历一遍条件相等的数据行!!!
+# print(row.id,row.title,session.query(Users).filter(Users.user_type_id == row.id).all())
